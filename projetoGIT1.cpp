7a14dbd90beae3be6080f537cbb54038318f145a
Mariana Faria <marianaantonioveiga@gmail.com>
2019-11-20 20:47:53 -0300

    Fiz alteraçaões

--- projetoGIT1.cpp
+++ projetoGIT1.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 
 /*
@@ -6,6 +8,7 @@
 */
 int contaLetras(char letra, string texto)
 {
+	
 }
 
 /*
@@ -20,13 +23,31 @@ void substituirLetra(char letra1,char letra2, string texto)
 */
 int contaPalavras(string palavra, string texto)
 {
+	size_t pos=texto.find(palavra);
+	int contador=0;
+	while(pos!=string::npos)
+	{
+		contador++;
+		texto=texto.substr(pos+1);
+		pos=texto.find(palavra);
+		
+	}
+	return contador;
 }
 
 /*
 	Substitui todas as ocorrencias de 'palavra1' por 'palavra2' em 'texto'
 */
-void substituirPalavras(string palavra1, string palavra2, string texto)
+string substituirPalavras(string palavra1, string palavra2, string texto)
 {
+	size_t pos=texto.find(palavra1);
+	while(pos!=string::npos)
+	{
+		texto.replace(pos,palavra1.length(),palavra2);
+		pos=texto.find(palavra1);
+	}
+	return texto;
+	
 }
 
 int main(){
